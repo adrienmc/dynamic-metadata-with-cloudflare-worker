c7f3127bf57e20e41aab8d43d8091b0b9a16d625
adrienmc <48948362+adrienmc@users.noreply.github.com>
2025-02-02 21:11:35 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export const config = {
   domainSource: "https://www.gritt.io/", // Your WeWeb app preview link
   patterns: [
       {
-          pattern: "investor/[^/]+",
+          pattern: "/investor/[^/]+",
           metaDataEndpoint: "https://x8zx-3roh-u9yx.e2.xano.io/api:lVInP5FG/profileMetadata/{profile}"
       }
   ]
